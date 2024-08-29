b419b947570c23ada78bd1ed39186708d71256d5
Vieira-Alex-EMF <alex.dasilva@studentfr.ch>
2024-08-29 11:00:57 +0200

    Nouveau Exercices

---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,17 @@
+package exercices.exercice03;
+
+/**
+ * TypesDeBase
+ */
+public class TypesDeBase {
+
+    public static void main(String[] args) {
+        
+        int age = 15 ;
+        int years = 10 + age;
+        String name = "Alex";
+        
+        System.out.println("Je m'appelle " + name + " et J'ai " + age + " ans. " + "Dans 10 ans j'aurai " + years + " ans.");
+
+    }
+}
