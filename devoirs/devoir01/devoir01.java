4ac503c7d2eb059d9b86d7cae6d2d2d3baf18114
Vieira-Alex-EMF <alex.dasilva@studentfr.ch>
2024-09-12 08:39:08 +0200

    devoir1

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -3,20 +3,24 @@ package devoirs.devoir01;
 public class devoir01 {
 
     public static void main(String[] args) {
-        
+
         int monAge = 30;
-        int estMajeur = 18;
+        boolean estMajeur;
 
         System.out.println("Je m'appelle Vieira Alex");
 
         System.out.println("J'ai " + monAge + " ans");
 
+        if (monAge >= 20) {
+            estMajeur = true;
 
-        if (estMajeur > monAge) { 
-            System.out.println("Je suis mineur");
-            
-        }else {
+        } else {
+            estMajeur = false;
+        }
+        if (estMajeur) {
             System.out.println("Je suis Majeur");
+        } else {
+            System.out.println("Je suis Mineur");
         }
     }
 }
---
+++ exercices/exercice08/ExercicesBoucles1/ExercicesBoucles1.java
@@ -0,0 +1,12 @@
+package exercices.exercice08.ExercicesBoucles1;
+
+/**
+ * ExercicesBoucles1
+ */
+public class ExercicesBoucles1 {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
