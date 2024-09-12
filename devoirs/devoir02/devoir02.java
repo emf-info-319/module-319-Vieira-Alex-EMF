4015f0fd33a318f81ab0b239bd0d253d27c9cc6b
Vieira-Alex-EMF <alex.dasilva@studentfr.ch>
2024-09-12 13:09:09 +0200

    12.09.2024

--- devoirs/devoir02/devoir02.java
+++ devoirs/devoir02/devoir02.java
@@ -12,9 +12,16 @@ public class devoir02 {
 
 
         System.out.println("Il y a " + remplissage + " litres à remplir. ");
+        System.out.println( "Le reservoir A a " + reservoirA + " litres" + " Le reservoir B a " + reservoirB + " litres");
 
         while (reservoirA < CAPACITE_RESERVOIR_A) {
-            
+            reservoirA = reservoirA + remplissage;
+            System.out.println("Voici le reservoir A : " + reservoirA + " Litres");
+            if (reservoirA == CAPACITE_RESERVOIR_A) {
+                
+                reservoirB = reservoirB + remplissage;
+                System.out.println("Voici le reservoir B : " + reservoirB + " Litres");
+            }
         }
 
     }
